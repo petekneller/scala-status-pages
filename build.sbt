8793ee84e14e58ba94f2ee8b7abbb665fd40ab90
Pete Kneller <Peter.Kneller@barclayswealth.com>
2016-07-20 21:35:13 +0100

    Forgot to add http4s module to the root build

--- build.sbt
+++ build.sbt
@@ -10,4 +10,6 @@ lazy val core = project in file("core")
 
 lazy val liftweb = (project in file("liftweb")).dependsOn(core)
 
-lazy val root = (project in file(".")).aggregate(core, liftweb)
+lazy val http4s = (project in file("http4s")).dependsOn(core)
+
+lazy val root = (project in file(".")).aggregate(core, liftweb, http4s)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
 
   val liftVersion = "2.6.2"
   val scalazVersion = "7.1.6"
+  val http4sVersion = "0.12.3"
 
   val argonaut         = "io.argonaut"                   %% "argonaut"                  % "6.1"
   val liftWebkit       = "net.liftweb"                   %% "lift-webkit"               % liftVersion
@@ -11,6 +12,6 @@ object Dependencies {
   val jodaTime         = "joda-time"                      % "joda-time"                 % "2.9.1"
   val jodaTimeConvert  = "org.joda"                       % "joda-convert"              % "1.8.1"
   val scalazCore       = "org.scalaz"                    %% "scalaz-core"               % scalazVersion
-  val pimpathon        = "com.github.stacycurl"          %% "pimpathon-core"            % "1.5.20"
-  val servletApi       = "javax.servlet"                  % "javax.servlet-api"         % "3.1.0"
+  val http4sCore       = "org.http4s"                    %% "http4s-core"               % http4sVersion
+  val http4sDsl        = "org.http4s"                    %% "http4s-dsl"                % http4sVersion
 }
